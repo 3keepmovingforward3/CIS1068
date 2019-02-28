914366f4e5b1417fed2c2bacee6d027c13771204
Benjamin Blouin <tug78140@temple.edu>
2019-02-28 10:18:44 -0500

    Everything works except append r

--- SouthieStyle/src/SouthieStyle.java
+++ SouthieStyle/src/SouthieStyle.java
@@ -1,4 +1,3 @@
-import java.util.regex.*;
 import java.io.*;
 import java.nio.file.*;
 import java.util.stream.Collectors;
@@ -8,29 +7,34 @@ import java.util.List;
 public class SouthieStyle {
 
     public static void main(String[] args) {
-        String regexYah = "(?<=ee|i)r\b"; //case sensitive positive lookbehind ending word boundary
-        String regexWah = "(?<=oo)r\b"; //case sensitive positive lookbehind ending word boundary
-        Pattern regexRhotic = Pattern.compile("[?i:aeiou](r)");
-        Pattern regexNonRhotic = Pattern.compile("[^\\s](?i:a)\\b");
-        Pattern regexVery = Pattern.compile("\\b(?i:very)\\b");
+        String regexYah = "(?<=ee|i)r\\b"; //case sensitive positive lookbehind "ee" or "i" ending word boundary, "r" replace
+        String regexWah = "(?<=oo)r\\b"; //case sensitive positive lookbehind "oo" ending word boundary, "r" replace
+        String regexRhotic = "(?i)(?<=:a|e|i|o|u)r"; //case insensitive positive lookbehind vowel word boundary, "r" replace
+        String regexNonRhotic = "(?<=\\w)a\\b";
+        String regexVery = "(?i)\\b(very)\\b"; // literal word bounded case insensitive "very", "wicked" replace
+
+        Path path = Paths.get("/Users/bblouin/Documents/GitHub/CIS1068/SouthieStyle/src/jaws.txt");
+        regexFindAndReplace(path, regexVery, "wicked");
+        regexFindAndReplace(path, regexYah,"yah");
+        regexFindAndReplace(path, regexWah,"wah");
+        regexFindAndReplace(path, regexRhotic,"h");
+        regexFindAndReplace(path, regexNonRhotic, "ar");
 
 
+        System.out.println("Find and replace done");
+    }
 
-            Path path = Paths.get("/Users/bblouin/Documents/GitHub/CIS1068/SouthieStyle/src/jaws.txt");
-
+    private static void regexFindAndReplace(Path path, String regex, String replacement){
         try{
             Stream<String> lines = Files.lines(path);
-            List<String> replaced = lines.map(line -> line.replaceAll(regexYah,"yah")).collect(Collectors.toList());
+            List<String> replaced = lines.map(line -> line.replaceAll(regex,replacement)).collect(Collectors.toList());
             Files.write(path,replaced);
             lines.close();
-
         }
         catch(IOException e){
             e.printStackTrace();
         }
-        System.out.println("Find and replace done");
     }
-
 
 
 }
