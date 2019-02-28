766c1c5870a035277562803eb9e389ef6549d5ab
Benjamin Blouin <tug78140@temple.edu>
2019-02-28 13:10:02 -0500

    updated source code

--- SouthieStyle/src/SouthieStyle.java
+++ SouthieStyle/src/SouthieStyle.java
@@ -1,3 +1,11 @@
+/*
+ * Benjamin Blouin
+ * CIS 1068
+ * Dr. Fiore
+ * Thanks to https://www.regular-expressions.info 
+ * 
+ */
+
 import java.io.*;
 import java.nio.file.*;
 import java.util.stream.Collectors;
@@ -7,19 +15,32 @@ import java.util.List;
 public class SouthieStyle {
 
     public static void main(String[] args) {
-        String regexYah = "(?<=ee|i)r\\b"; //case sensitive positive lookbehind "ee" or "i" ending word boundary, "r" replace
-        String regexWah = "(?<=oo)r\\b"; //case sensitive positive lookbehind "oo" ending word boundary, "r" replace
-        String regexRhotic = "(?i)(?<=:a|e|i|o|u)r"; //case insensitive positive lookbehind vowel word boundary, "r" replace
+    	//case sensitive positive look-behind "ee" or "i" ending word boundary, "r" replace
+    	//
+    	String regexYah = "(?<=ee|i)r\\b"; 
+    	
+    	//case sensitive positive look-behind "oo" ending word boundary, "r" replace
+    	//
+        String regexWah = "(?<=oo)r\\b"; 
+        
+        //case insensitive positive look-behind vowel word boundary, "r" replace
+        //
+        String regexRhotic = "(?i)(?<=:a|e|i|o|u)r"; 
+        
+        //case sensitive positive look-behind word-character ending "a" word-boundary, replace "ar"
+        //
         String regexNonRhotic = "(?<=\\w)a\\b";
-        String regexVery = "(?i)\\b(very)\\b"; // literal word bounded case insensitive "very", "wicked" replace
+        
+        //literal word bounded case insensitive "very", "wicked" replace
+        //
+        String regexVery = "(?i)\\b(very)\\b"; 
 
-        Path path = Paths.get("/Users/bblouin/Documents/GitHub/CIS1068/SouthieStyle/src/jaws.txt");
+        Path path = Paths.get("/Users/bblouin/eclipse-workspace/SouthieStyle/jaws.txt");
         regexFindAndReplace(path, regexVery, "wicked");
         regexFindAndReplace(path, regexYah,"yah");
         regexFindAndReplace(path, regexWah,"wah");
         regexFindAndReplace(path, regexRhotic,"h");
         regexFindAndReplace(path, regexNonRhotic, "ar");
-
 
         System.out.println("Find and replace done");
     }
