8b0de7e1d37a0c4975afb021836dbabc6275e0cc
Benjamin Blouin <tug78140@temple.edu>
2019-03-04 11:05:21 -0500

    final solution SouthieStyle

--- SouthieStyle/SouthieStyle.java
+++ SouthieStyle/SouthieStyle.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class SouthieStyle {
 
-	static final String FILENAME = "jaws.txt";
+	static final String FILENAME = "1513-0.txt";
     
 	public static void main(String[] args) {
     	
