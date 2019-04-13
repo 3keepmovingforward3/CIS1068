75481bac2b95bf33dee3b28275620d3d693576c0
Benjamin Blouin <tug78140@temple.edu>
2019-04-13 11:44:46 -0400

    init

---
+++ hangman/src/hangman/hangman.java
@@ -0,0 +1,36 @@
+package hangman;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class hangman {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Objects.requireNonNull(initWordList()));
+
+
+    }
+
+
+    private static List<String> initWordList() {
+
+        String fileName = "word_list.txt";
+
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+            //br returns as stream and convert it into a List
+            return br.lines().collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    return null;
+
+    }
+
+
+}
