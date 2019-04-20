873d618380e3f8f8296139e12317a7e48c13142d
Benjamin Blouin <3keepmovingforward3@gmail.com>
2019-04-20 08:20:06 -0400

    cleanup

--- Yates' Big File Sorter/src/sorter.java
+++ Yates' Big File Sorter/src/sorter.java
@@ -5,41 +5,40 @@ public class sorter {
     public static void main(String[] args) throws IOException{
         final int totalLines = 149035;
         int linesPerFile = totalLines/20;
-        String fileName = "/Users/bblouin/Documents/GitHub/CIS1068/Yates' Big File Sorter/src/Aesop_Shakespeare_Shelley_Twain.txt";
-        int j = 0, k = 0, l = 0;
+        String fileName = "/Users/bblouin/Documents/GitHub/CIS1068/Yates' " +
+                "Big File Sorter/src/Aesop_Shakespeare_Shelley_Twain.txt";
+        int j = 0, k = 0, l = 0, h=0;
 
 
         FileReader fileReader = new FileReader(fileName);
         BufferedReader br = new BufferedReader(fileReader);
 
+        while(h<20) {
 
-            int h = 0;
+            File file = new File("/Users/bblouin/Documents/GitHub/CIS1068/Yates' " +
+                    "Big File Sorter/src/temp_" + l + "_file.txt");
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
 
-            while(h<20) {
-
-                File file = new File("/Users/bblouin/Documents/GitHub/CIS1068/Yates' Big File Sorter/src/temp_" + l + "_file.txt");
-                FileWriter fw = new FileWriter(file);
-                BufferedWriter bw = new BufferedWriter(fw);
-
-                String[] stringList = new String[linesPerFile];
+            String[] stringList = new String[linesPerFile];
 
 
-                for (j=0; j < linesPerFile; j++) {
-                    stringList[j] = br.readLine();
-                }
-                br.mark(0);
-                Arrays.sort(stringList);
-
-                for (k=0; k < linesPerFile; k++) {
-                    if (!stringList[k].equals("")) {
-                        bw.write(stringList[k].trim());
-                        bw.newLine();
-                    }
-
-                }
-                l++;
-                h++;
+            for (j=0; j < linesPerFile; j++) {
+                stringList[j] = br.readLine();
             }
+            br.mark(0);
+            Arrays.sort(stringList);
+
+            for (k=0; k < linesPerFile; k++) {
+                if (!stringList[k].equals("")) {
+                    bw.write(stringList[k].trim());
+                    bw.newLine();
+                }
+
+            }
+            l++;
+            h++;
+        }
 
     }
 }
